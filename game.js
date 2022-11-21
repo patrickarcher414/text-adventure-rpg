8449debeb73e4f069f45ddd331cd4bafe906478c
patrickarcher414 <patrickarcher414@gmail.com>
2022-11-21 15:22:43 -0600

    showText working, finishing textNodes

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ function startGame() {
 }
 
 function showOption(option){
-  return true;
+  return option.requiredState == null || option.requiredState(state);
 }
 
 function showTextNode(textNodeIndex) {
@@ -31,7 +31,12 @@ function showTextNode(textNodeIndex) {
 }
 
 function selectOption(option) {
-
+  const nextTextNodeId = option.nextText;
+  if(nextTextNodeId <= 0){
+    return startGame()
+  }
+  state = Object.assign(state, option.setState)
+  showTextNode(nextTextNodeId)
 }
 
 const textNodes = [
@@ -75,6 +80,39 @@ const textNodes = [
       {
         text: 'Leave the blade',
         nextText: 5
+      }
+    ]
+  },
+  {
+    id: 4,
+    text: 'You walk down the long hall towards the green light and as you get closer the light grows brighter. When you reach the end of the hall you see the light is coming from an iron brazier, green flames lick the air reaching eye level. As you stand before it a silhouette starts to form in the flames. A humanoid figure about chest height, but with long pointy ears and a slouched posture. The silhouette darkens and the figure lurches forward, leaving the brazier and flames. It moves towards you arms raised. What do you do?',
+    options: [
+      {
+        text: 'Turn and run!',
+        nextText: 6
+      },
+      {
+        text: 'Punch the shadowy figure.',
+        nextText: 7
+      },
+      {
+        text: 'Slash the shadowy figure with your blade.',
+        requiredState: (currentState) => currentState.smallBlade,
+        nextText: 8
+      },
+      {
+        text: 'Do nothing.',
+        nextText: 9
+      }
+    ]
+  },
+  {
+    id: 9,
+    text: 'The shadowy figure raises it\'s dark twisted claws and swipes at you vigorously. A cold numbing pain fills your body growing out from where you were cut. You begin to feel drowsy and fall to your knees... A tragic fate befell you. You have unfortunately perished.',
+    options: [
+      {
+        text: 'Restart',
+        nexText: -1
       }
     ]
   }
